0e2ec8b6b09f1ac5ade8050fe175219df42bb2bc
Hannah Guo <hguo2102@gmail.com>
2024-12-17 17:12:29 -0500

    firebase

--- components/DealsList.tsx
+++ components/DealsList.tsx
@@ -83,9 +83,65 @@ export default function DealsList() {
             },
             notes: "Needs app, limit 2 per app"
         },
+        {
+            id: 'test123', storeID: 'The Alley', dealType: 'single', // TODO: make storeID real
+            discounts: [{
+                discountType: "percentage",
+                discountValue: 50
+            }],
+            drinks: [{
+                name: "Royal No. 9 Milk Tea",
+                size: "any",
+                type: "milktea",
+            }],
+            promoPeriod: {
+                startDate: "always",
+                endDate: "always",
+                condition: {
+                    date: 9,
+                }
+            },
+            notes: "Needs app, limit 2 per app"
+        },
     ];
 
-    const stores = {}
+
+    const storeDeals: StoreDeal[] = [{
+        storeID: 'Now Tea',
+        id: "now-tea-boba",
+        condition: {
+            id: "bobatime-card",
+            clause: "Needs UW BobaTime Card",
+        },
+        promoPeriod: {
+            startDate: "always",
+            endDate: "always",
+        },
+        discount: {
+            discountType: "percentage",
+            discountValue: 10
+        }
+    }]
+
+
+    const stores: Store[] = [
+        {
+            id: "Now Tea",
+            name: "Now Tea",
+            address: "280 Lester St #106, Waterloo, ON N2L 0G2"
+        }
+    ]
+
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const testDoc = await setDoc(doc(db, "cities", "LA"), {
+    //             name: "Los Angeles",
+    //             state: "CA",
+    //             country: "USA"
+    //         });
+    //     };
+    //     fetchData();
+    // }, []);
 
     return (
         <View style={styles.listContainer}>
---
+++ firebase/app/firebaseConfig.ts
@@ -0,0 +1,20 @@
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCtjjbGJgEME2fhOfRRL1ehPtSYHbNV180",
+  authDomain: "so-boba-after.firebaseapp.com",
+  projectId: "so-boba-after",
+  storageBucket: "so-boba-after.firebasestorage.app",
+  messagingSenderId: "434274563582",
+  appId: "1:434274563582:web:6bd8fa3852296359fafcaf",
+  measurementId: "G-CGPNK9R9SS"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
