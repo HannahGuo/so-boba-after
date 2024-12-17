86d5484297eabd158b32bc984198c817b54b48ce
Hannah Guo <hguo2102@gmail.com>
2024-12-17 17:28:47 -0500

    routing fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,7 @@
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
-import DealsList from '@/components/DealsList';
-import { ThemedText } from '@/components/ThemedText';
-import { Colors } from '@/constants/Colors';
-import { ImageBackground, StyleSheet, View } from 'react-native';
+import { Stack } from 'expo-router';
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -16,29 +13,9 @@ export default function RootLayout() {
   })
 
   return (
-    <View style={styles.mainContainer}>
-      <ImageBackground source={require('../assets/images/bobaHeader.svg')} style={styles.headerImage}/>
-      <View style={styles.titleContainer}>
-        <ThemedText type='title'>...so boba after?</ThemedText>
-      </View>
-      <DealsList/>
-    </View>
+    <Stack screenOptions={{headerShown: false}}>
+      <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="add" options={{ title: 'Add' }} />
+    </Stack>
   );
 }
-
-const styles = StyleSheet.create({
-  mainContainer: {
-    height: '100%',
-    backgroundColor: Colors.light.background,
-    display: 'flex',
-  },
-  titleContainer: {
-    marginTop: 10,
-    marginLeft: 20,
-  },
-  headerImage: {
-    position: 'absolute',
-    top: -40,
-    left: -20,
-  }
-});
---
+++ app/add/index.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/components/ThemedText";
+import { View } from "react-native";
+
+export default function Add() {
+    return (
+        <View>
+            <ThemedText>Hellooooo World!</ThemedText>
+        </View>
+    );
+}
