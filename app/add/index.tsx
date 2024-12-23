4a60c90e2043a8a1d535cff5046b1ae12b902b25
Hannah Guo <hguo2102@gmail.com>
2024-12-23 13:29:20 -0500

    tabby

--- app/add/index.tsx
+++ app/add/index.tsx
@@ -9,36 +9,54 @@ import AddBobaDeal from "./AddBobaDeal"
 import AddStoreDeal from "./AddStoreDeal"
 
 export default function Add() {
+	const [tabIndex, setTabIndex] = React.useState(0)
+
 	return (
 		<ScrollView style={styles.mainContainer}>
 			<Header page="add" />
-			<View style={styles.formContainer}>
-				<Tabs>
-					<TabList style={{ borderBottom: "1px solid white" }}>
-						<Tab
-							style={{ backgroundColor: Colors.shared.bobaBrown }}
-						>
-							<ThemedText type="subtitle">
-								Add Boba Deal
-							</ThemedText>
-						</Tab>
-						<Tab
-							style={{ backgroundColor: Colors.shared.bobaBrown }}
-						>
-							<ThemedText type="subtitle">
-								Add Store Deal
-							</ThemedText>
-						</Tab>
-					</TabList>
+			<Tabs>
+				<TabList style={styles.tabContainer}>
+					<Tab
+						style={{
+							...styles.tabHeader,
+							...(tabIndex === 0
+								? {
+										backgroundImage: `linear-gradient(to top, ${Colors.shared.bobaBrown}, 95%, ${Colors.shared.bobaBrownLight})`,
+								  }
+								: {
+										backgroundImage: `linear-gradient(to top, ${Colors.shared.bobaBrownLight}, 95%, ${Colors.shared.bobaBrownLight})`,
+								  }),
+						}}
+						onClick={() => setTabIndex(0)}
+					>
+						<ThemedText type="subtitle">Add Boba Deal</ThemedText>
+					</Tab>
+					<Tab
+						style={{
+							...styles.tabHeader,
+							...(tabIndex === 1
+								? {
+										backgroundImage: `linear-gradient(to top, ${Colors.shared.bobaBrown}, 95%, ${Colors.shared.bobaBrownLight})`,
+								  }
+								: {
+										backgroundImage: `linear-gradient(to top, ${Colors.shared.bobaBrownLight}, 95%, ${Colors.shared.bobaBrownLight})`,
+								  }),
+						}}
+						onClick={() => setTabIndex(1)}
+					>
+						<ThemedText type="subtitle">Add Store Deal</ThemedText>
+					</Tab>
+				</TabList>
 
+				<View style={styles.formContainer}>
 					<TabPanel>
 						<AddBobaDeal />
 					</TabPanel>
 					<TabPanel>
 						<AddStoreDeal />
 					</TabPanel>
-				</Tabs>
-			</View>
+				</View>
+			</Tabs>
 		</ScrollView>
 	)
 }
@@ -50,12 +68,25 @@ const styles = StyleSheet.create({
 	},
 	formContainer: {
 		display: "flex",
-		backgroundImage: `linear-gradient(to top, ${Colors.shared.bobaBrown}, 95%, ${Colors.shared.bobaBrownLight})`,
 		padding: 40,
 		width: "90%",
 		margin: "auto",
-		marginTop: 120,
-		borderRadius: 20,
+		borderTopLeftRadius: 0,
+		borderTopRightRadius: 20,
+		borderBottomLeftRadius: 20,
+		borderBottomRightRadius: 20,
 		marginBottom: 40,
+		backgroundColor: Colors.shared.bobaBrown,
+	},
+	tabContainer: {
+		borderBottomColor: "white",
+		marginBottom: 0,
+		marginTop: 120,
+		marginLeft: "5%",
+	},
+	tabHeader: {
+		borderTopLeftRadius: 10,
+		borderTopRightRadius: 10,
+		borderWidth: 0,
 	},
 })
