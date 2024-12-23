2c06e6f9c9666ba6fa7e61effeb074e912d784db
Hannah Guo <hguo2102@gmail.com>
2024-12-22 22:53:05 -0500

    linting... maybe now

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,21 @@
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from "expo-font"
+import * as SplashScreen from "expo-splash-screen"
 
-import { Stack } from 'expo-router';
-SplashScreen.preventAutoHideAsync();
+import { Stack } from "expo-router"
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    LondrinaSolid: require('../assets/fonts/LondrinaSolid-Regular.ttf'),
-    CourierPrime: require('../assets/fonts/CourierPrime-Regular.ttf'),
-    CourierPrimeBold: require('../assets/fonts/CourierPrime-Bold.ttf'),
-    CourierPrimeItalic: require('../assets/fonts/CourierPrime-Italic.ttf'),
-  })
+	const [loaded] = useFonts({
+		LondrinaSolid: require("../assets/fonts/LondrinaSolid-Regular.ttf"),
+		CourierPrime: require("../assets/fonts/CourierPrime-Regular.ttf"),
+		CourierPrimeBold: require("../assets/fonts/CourierPrime-Bold.ttf"),
+		CourierPrimeItalic: require("../assets/fonts/CourierPrime-Italic.ttf"),
+	})
 
-  return (
-    <Stack screenOptions={{headerShown: false}}>
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
-      <Stack.Screen name="add" options={{ title: 'Add' }} />
-    </Stack>
-  );
+	return (
+		<Stack screenOptions={{ headerShown: false }}>
+			<Stack.Screen name="index" options={{ title: "Home" }} />
+			<Stack.Screen name="add" options={{ title: "Add" }} />
+		</Stack>
+	)
 }
